647c947920f2ca47795b723d709e40fa127f2d8b
linduxed <linduxed@gmail.com>
2012-07-06 00:57:51 +0200

    The four loops merged into one loop.

--- kalaha.hs
+++ kalaha.hs
@@ -51,52 +51,25 @@ moveMarbles ((listOfPots, landedInStore), marblesInHand, mustContinue) startingP
  - hand).
  - The top case in each of the loop sections only happens the first time the
  - loop is called (it's the only time no marbles are held).
- -
- - TODO: there must be a better way than having four near identical loops.
  -}
 moveOneLap :: [Pot] -> Int -> Int -> (([Pot], Bool), Int, Bool)
 moveOneLap listOfPots startingPot startingMarblesInHand = ((modifiedPots, landedInStore), marblesLeftInHand, mustDoAnotherLap) where
-    modifiedPots       = untouchedFirstPots ++ moveLoop toTraverse startingMarblesInHand
-    landedInStore      = storeLoop toTraverse startingMarblesInHand
-    marblesLeftInHand  = marbleLoop toTraverse startingMarblesInHand
-    mustDoAnotherLap   = continuationLoop toTraverse startingMarblesInHand
-    untouchedFirstPots = take (startingPot - 1) listOfPots
-    toTraverse         = drop (startingPot - 1) listOfPots
+    (newPots, marblesLeftInHand, mustDoAnotherLap, landedInStore) = moveLoop toTraverse startingMarblesInHand []
+    (untouchedFirstPots, toTraverse)                              = splitAt (startingPot - 1) listOfPots
+    modifiedPots                                                  = untouchedFirstPots ++ newPots
 
-    moveLoop [] _ = []
-    moveLoop (x:xs) marblesInHand
-        | marblesInHand == 0                         = returnEmptyPot x : moveLoop xs (marbleCount x)
-        | marblesInHand > 1                          = returnPotWithOneMoreMarble x : moveLoop xs (marblesInHand - 1)
-        | isStore x && marblesInHand == 1            = returnPotWithOneMoreMarble x : xs
-        | (not $ isPotEmpty x) && marblesInHand == 1 = returnEmptyPot x : moveLoop xs (marbleCount x + 1)
-        | isPotEmpty x && marblesInHand == 1         = returnPotWithOneMoreMarble x : xs
-        where
-            returnPotWithOneMoreMarble pot = pot { marbleCount = (marbleCount pot + 1) }
-            returnEmptyPot pot             = pot { marbleCount = 0 }
-
-    marbleLoop [] marblesInHand = marblesInHand
-    marbleLoop (x:xs) marblesInHand
-        | marblesInHand == 0                         = marbleLoop xs (marbleCount x)
-        | marblesInHand > 1                          = marbleLoop xs (marblesInHand - 1)
-        | isStore x && marblesInHand == 1            = 0
-        | (not $ isPotEmpty x) && marblesInHand == 1 = marbleLoop xs (marbleCount x + 1)
-        | isPotEmpty x && marblesInHand == 1         = 0
-
-    continuationLoop [] _ = True
-    continuationLoop (x:xs) marblesInHand
-        | marblesInHand == 0                         = continuationLoop xs (marbleCount x)
-        | marblesInHand > 1                          = continuationLoop xs (marblesInHand - 1)
-        | isStore x && marblesInHand == 1            = False
-        | (not $ isPotEmpty x) && marblesInHand == 1 = continuationLoop xs (marbleCount x + 1)
-        | isPotEmpty x && marblesInHand == 1         = False
-
-    storeLoop [] _ = False
-    storeLoop (x:xs) marblesInHand
-        | marblesInHand == 0                         = storeLoop xs (marbleCount x)
-        | marblesInHand > 1                          = storeLoop xs (marblesInHand - 1)
-        | isStore x && marblesInHand == 1            = True
-        | (not $ isPotEmpty x) && marblesInHand == 1 = storeLoop xs (marbleCount x + 1)
-        | isPotEmpty x && marblesInHand == 1         = False
+moveLoop [] marblesInHand outList = (reverse outList, marblesInHand, True, False)
+moveLoop (x:xs) marblesInHand outList
+    | marblesInHand == 0 = moveLoop xs (marbleCount x)     (returnEmptyPot x : outList)
+    | marblesInHand >  1 = moveLoop xs (marblesInHand - 1) (returnPotWithOneMoreMarble x : outList)
+    | marblesInHand /= 1 = error "marblesInHand was neither 0 nor >1, but somehow isn't 1"
+    | isStore x          = (finishedPots, 0, False, True)
+    | isPotEmpty x       = (finishedPots, 0, False, False)
+    | otherwise          = moveLoop xs (marbleCount x + 1) (returnEmptyPot x : outList)
+    where
+        returnPotWithOneMoreMarble pot = pot { marbleCount = (marbleCount pot + 1) }
+        returnEmptyPot pot             = pot { marbleCount = 0 }
+        finishedPots                   = (reverse outList) ++ [returnPotWithOneMoreMarble x] ++ xs
 -- }}}
 
 -- Starting move branching {{{
