8b3b86de64bd8689eb822e3a481df952bf97306e
Mikael Muszynski <linduxed@gmail.com>
2013-10-18 19:29:18 +0200

    Write generateBoard.

--- Board.hs
+++ Board.hs
@@ -7,3 +7,10 @@ data Pot = Pot {
 } deriving (Show)
 
 type Board = [Pot]
+
+generateBoard :: Int -> Board
+generateBoard startingMarbles = map generatePot [1..13]
+  where
+    generatePot position
+        | position /= 7 = Pot position startingMarbles False
+        | otherwise     = Pot position 0 True
