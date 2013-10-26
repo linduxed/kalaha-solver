6d7729320f644d48b145bc5504e057f0f0c24cbd
Mikael Muszynski <linduxed@gmail.com>
2013-10-26 22:38:31 +0200

    Use Either as return type for error handling.

--- Kalaha/Movement.hs
+++ Kalaha/Movement.hs
@@ -2,5 +2,5 @@ module Kalaha.Movement where
 
 import Kalaha.Board
 
-makeMove :: Board -> Int -> Board
+makeMove :: Board -> Int -> Either String Board
 makeMove = undefined
--- spec/MovementSpec.hs
+++ spec/MovementSpec.hs
@@ -10,18 +10,18 @@ main = hspec $ do
         context "picking a marble which lands you in the store" $ do
             it "returns an altered board" $ do
                 let startingBoard = generateBoard 2
-                let expectedBoard = [ Pot 1 2 False
-                                    , Pot 2 2 False
-                                    , Pot 3 2 False
-                                    , Pot 4 2 False
-                                    , Pot 5 0 False
-                                    , Pot 6 3 False
-                                    , Pot 7 1 True
-                                    , Pot 8 2 False
-                                    , Pot 9 2 False
-                                    , Pot 10 2 False
-                                    , Pot 11 2 False
-                                    , Pot 12 2 False
-                                    , Pot 13 2 False ]
+                let expectedBoard = Right [ Pot 1 2 False
+                                          , Pot 2 2 False
+                                          , Pot 3 2 False
+                                          , Pot 4 2 False
+                                          , Pot 5 0 False
+                                          , Pot 6 3 False
+                                          , Pot 7 1 True
+                                          , Pot 8 2 False
+                                          , Pot 9 2 False
+                                          , Pot 10 2 False
+                                          , Pot 11 2 False
+                                          , Pot 12 2 False
+                                          , Pot 13 2 False ]
 
                 makeMove startingBoard 5 `shouldBe` expectedBoard
