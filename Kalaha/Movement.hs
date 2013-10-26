fdea7d72bc624b06565bd56c0c0fb0f4eac203a6
Mikael Muszynski <linduxed@gmail.com>
2013-10-26 22:56:34 +0200

    Make error tests pass.
    All the Left tests pass, but none of the Right ones.

--- Kalaha/Movement.hs
+++ Kalaha/Movement.hs
@@ -3,4 +3,10 @@ module Kalaha.Movement where
 import Kalaha.Board
 
 makeMove :: Board -> Int -> Either String Board
-makeMove = undefined
+makeMove board startPot
+    | startPot < 1 || startPot > 13  = Left "makeMove: Starting position \
+                                            \outside of board."
+    | startPot == 7                  = Left "makeMove: Can't start from store."
+    | startPot < 7 || startPot <= 13 = Left "makeMove: Starting position on \
+                                            \opponent's side of board."
+    | otherwise                      = Right $ generateBoard 2
