a8af6d32942c60aa2cfd2f978606204693621f12
Mikael Muszynski <linduxed@gmail.com>
2013-10-27 10:29:38 +0100

    Change an || to &&.

--- Kalaha/Movement.hs
+++ Kalaha/Movement.hs
@@ -7,6 +7,6 @@ makeMove board startPot
     | startPot < 1 || startPot > 13  = Left "makeMove: Starting position \
                                             \outside of board."
     | startPot == 7                  = Left "makeMove: Can't start from store."
-    | startPot < 7 || startPot <= 13 = Left "makeMove: Starting position on \
+    | startPot > 7 && startPot <= 13 = Left "makeMove: Starting position on \
                                             \opponent's side of board."
     | otherwise                      = Right $ generateBoard 2
